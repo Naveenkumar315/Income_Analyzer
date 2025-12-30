cd60703b22e2e000cd5eda5e492f7206ad0419d1
Naveen <naveenkumarm572@gmail.com>
2025-12-30 19:16:55 +0530

    origin issue fix

--- backend/main.py
+++ backend/main.py
@@ -68,9 +68,7 @@ app.include_router(admin.router)
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  # frontend
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
